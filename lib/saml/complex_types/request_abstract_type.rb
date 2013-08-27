47dc6fba6773d50f3b777f1f7279a5e05be7779d
Danny van der Heiden <dvanderheiden@digidentity.eu>
2013-08-27 14:59:57 +0200

    Remove custom tag of a Signature element in a RequestAbstractType, because it's the default.

--- lib/saml/complex_types/request_abstract_type.rb
+++ lib/saml/complex_types/request_abstract_type.rb
@@ -19,7 +19,7 @@ module Saml
         attribute :destination, String, :tag => "Destination"
         element :issuer, String, :namespace => 'saml', :tag => "Issuer"
 
-        has_one :signature, Saml::Elements::Signature, :tag => "Signature"
+        has_one :signature, Saml::Elements::Signature
         has_one :extensions, Saml::Elements::Extensions
 
         validates :_id, :version, :issue_instant, :presence => true
