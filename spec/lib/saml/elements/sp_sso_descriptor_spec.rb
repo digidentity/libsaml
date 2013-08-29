76328989956d4693f494fa06f4a27d7dea811fd6
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-08-29 17:46:13 +0200

    Added the AttributeConsumingServices to the SP SSO descriptor element.

--- spec/lib/saml/elements/sp_sso_descriptor_spec.rb
+++ spec/lib/saml/elements/sp_sso_descriptor_spec.rb
@@ -8,4 +8,19 @@ describe Saml::Elements::SPSSODescriptor do
     end
 
   end
+
+  describe "Optional fields" do
+    [:attribute_consuming_services].each do |field|
+      it "should have the #{field} field" do
+        subject.should respond_to(field)
+      end
+
+      it "should allow #{field} to blank" do
+        subject.send("#{field}=", nil)
+        subject.errors.entries.should == [] #be_valid
+        subject.send("#{field}=", "")
+        subject.errors.entries.should == [] #be_valid
+      end
+    end
+  end
 end
