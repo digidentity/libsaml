87ea2ebfb579333e114e86a7b4f8cb11bd00934d
Jonathan Hicks <jonathan.r.hicks@gmail.com>
2013-12-13 11:29:05 -0500

    Adding in a provider method to an xml assertion in order for the assertion to be signed without the entire message being signed.

--- spec/lib/saml/util_spec.rb
+++ spec/lib/saml/util_spec.rb
@@ -11,37 +11,76 @@ end
 
 describe Saml::Util do
   let(:service_provider) { ServiceProvider.new }
-  let(:message) { FactoryGirl.build :authn_request, issuer: service_provider.entity_id }
   let(:signed_message) { "signed xml" }
+  let(:message) { FactoryGirl.build :authn_request, issuer: service_provider.entity_id }
 
-  describe ".sign_xml" do
-    it "calls add_signature on the specified message" do
-      message.should_receive(:add_signature)
-      described_class.sign_xml message
-    end
+  describe "authn_request" do
+    describe ".sign_xml" do
+      it "calls add_signature on the specified message" do
+        message.should_receive(:add_signature)
+        described_class.sign_xml message
+      end
 
-    it "creates a new signed document" do
-      Xmldsig::SignedDocument.should_receive(:new).with(any_args).and_return stub.as_null_object
-      described_class.sign_xml message
-    end
+      it "creates a new signed document" do
+        Xmldsig::SignedDocument.should_receive(:new).with(any_args).and_return stub.as_null_object
+        described_class.sign_xml message
+      end
 
-    context "when a block is given" do
-      it "sign is called on the signed document, not on the provider" do
-        message.provider.should_not_receive(:sign)
-        Xmldsig::SignedDocument.any_instance.should_receive(:sign).and_return signed_message
+      context "when a block is given" do
+        it "sign is called on the signed document, not on the provider" do
+          message.provider.should_not_receive(:sign)
+          Xmldsig::SignedDocument.any_instance.should_receive(:sign).and_return signed_message
 
-        described_class.sign_xml(message) do |data, signature_algorithm|
-          service_provider.sign signature_algorithm, data
+          described_class.sign_xml(message) do |data, signature_algorithm|
+            service_provider.sign signature_algorithm, data
+          end
+        end
+      end
+
+      context "without specifiying a block" do
+        it "sign is called on the provider of the specified message" do
+          Xmldsig::SignedDocument.any_instance.should_receive(:sign).and_yield(stub, stub)
+          message.provider.should_receive(:sign).and_return signed_message
+
+          described_class.sign_xml message
         end
       end
     end
+  end
 
-    context "without specifiying a block" do
-      it "sign is called on the provider of the specified message" do
-        Xmldsig::SignedDocument.any_instance.should_receive(:sign).and_yield(stub, stub)
-        message.provider.should_receive(:sign).and_return signed_message
+  describe "assertion" do
+    let(:assertion) { FactoryGirl.build :assertion, issuer: service_provider.entity_id }
+    let(:signed_assertion) { "signed xml" }
 
-        described_class.sign_xml message
+    describe ".sign_xml" do
+      it "calls add_signature on the specified assertion" do
+        assertion.should_receive(:add_signature)
+        described_class.sign_xml assertion
+      end
+
+      it "creates a new signed document" do
+        Xmldsig::SignedDocument.should_receive(:new).with(any_args).and_return stub.as_null_object
+        described_class.sign_xml assertion
+      end
+
+      context "when a block is given" do
+        it "sign is called on the signed document, not on the provider" do
+          assertion.provider.should_not_receive(:sign)
+          Xmldsig::SignedDocument.any_instance.should_receive(:sign).and_return signed_assertion
+
+          described_class.sign_xml(assertion) do |data, signature_algorithm|
+            service_provider.sign signature_algorithm, data
+          end
+        end
+      end
+
+      context "without specifiying a block" do
+        it "sign is called on the provider of the specified assertion" do
+          Xmldsig::SignedDocument.any_instance.should_receive(:sign).and_yield(stub, stub)
+          assertion.provider.should_receive(:sign).and_return signed_assertion
+
+          described_class.sign_xml assertion
+        end
       end
     end
   end
