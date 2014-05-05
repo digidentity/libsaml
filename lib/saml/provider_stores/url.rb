7938ac38434b1f0bfcef971c50c17a175844856a
Benoist <bclaassen@digidentity.eu>
2014-05-05 12:25:41 +0200

    fixed alias method

--- lib/saml/provider_stores/url.rb
+++ lib/saml/provider_stores/url.rb
@@ -2,16 +2,17 @@ module Saml
   module ProviderStores
     class Url
       attr_accessor :providers
+      class << self
+        def find_by_metadata_location(entity_id)
+          metadata          = Saml::Util.download_metadata_xml(entity_id)
+          entity_descriptor = Saml::Elements::EntityDescriptor.parse(metadata, single: true)
+          type              = entity_descriptor.sp_sso_descriptor.present? ? "service_provider" : "identity_provider"
 
-      def self.find_by_metadata_location(entity_id)
-        metadata          = Saml::Util.download_metadata_xml(entity_id)
-        entity_descriptor = Saml::Elements::EntityDescriptor.parse(metadata, single: true)
-        type              = entity_descriptor.sp_sso_descriptor.present? ? "service_provider" : "identity_provider"
+          BasicProvider.new(entity_descriptor, nil, type)
+        end
 
-        BasicProvider.new(entity_descriptor, nil, type)
+        alias_method :find_by_entity_id, :find_by_metadata_location
       end
-
-      alias_method :find_by_entity_id, :find_by_metadata_location
     end
   end
 end
