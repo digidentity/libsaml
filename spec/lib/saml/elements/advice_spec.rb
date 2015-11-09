df4b153294758c04f7adf7969d7a1db784cfdeec
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2015-11-09 09:39:44 +0100

    Added the AssertionIDRef element to the AdviceType.

--- spec/lib/saml/elements/advice_spec.rb
+++ spec/lib/saml/elements/advice_spec.rb
@@ -18,6 +18,14 @@ describe Saml::Elements::Advice do
     let(:advice_type_xml) { File.read(File.join('spec','fixtures','advice_type.xml')) }
     let(:advice)     { described_class.parse(advice_type_xml, single: true) }
 
+    it 'parses all the AssertionIDRefs' do
+      expect(advice.assertion_id_refs.count).to eq 2
+    end
+
+    it 'has the correct AssertionIDRef value' do
+      expect(advice.assertion_id_refs.first).to eq '_cf45463f265373cd3a54665ef40e033b34365e24'
+    end
+
     it 'parses all the Assertions' do
       expect(advice.assertions.count).to eq 2
     end
