d3628802d9dbe9a7a85195ba157b8f663462f33e
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2015-11-30 10:12:51 +0100

    Added the optional NotOnOrAfter on a LogoutRequest.

--- lib/saml/logout_request.rb
+++ lib/saml/logout_request.rb
@@ -3,6 +3,9 @@ module Saml
     include Saml::ComplexTypes::RequestAbstractType
 
     tag "LogoutRequest"
+
+    attribute :not_on_or_after, Time, :tag => "NotOnOrAfter", :on_save => lambda { |val| val.utc.xmlschema if val.present? }
+
     element :name_id, String, :tag => "NameID", :namespace => 'saml'
 
     validates :name_id, :presence => true
