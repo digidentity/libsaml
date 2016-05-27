23aac572e48294f951265e73de0705491ff3aeaa
Yong Wen Chua <chua_yong_wen@ida.gov.sg>
2016-05-27 09:44:37 +0800

    Add more possible `AuthnContextClassRef` values

--- lib/saml.rb
+++ lib/saml.rb
@@ -83,12 +83,52 @@ module Saml
     MOBILE_TWO_FACTOR_UNREGISTERED = 'urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorUnregistered'
     MOBILE_TWO_FACTOR_CONTRACT     = 'urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorContract'
     MOBILE_SMARTCARD_PKI           = 'urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI'
+    INTERNET_PROTOCOL              = 'urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocol'
+    INTERNET_PROTOCOL_PASSWORD     = 'urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocolPassword'
+    KERBEROS                       = 'urn:oasis:names:tc:SAML:2.0:ac:classes:Kerberos'
+    MOBILE_ONE_FACTOR_UNREGISTERED = 'urn:oasis:names:tc:SAML:2.0:ac:classes:MobileOneFactorUnregistered'
+    MOBILE_ONE_FACTOR_CONTRACT     = 'urn:oasis:names:tc:SAML:2.0:ac:classes:MobileOneFactorContract'
+    PASSWORD                       = 'urn:oasis:names:tc:SAML:2.0:ac:classes:Password'
+    PREVIOUS_SESSION               = 'urn:oasis:names:tc:SAML:2.0:ac:classes:PreviousSession'
+    X509                           = 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'
+    PGP                            = 'urn:oasis:names:tc:SAML:2.0:ac:classes:PGP'
+    SPKI                           = 'urn:oasis:names:tc:SAML:2.0:ac:classes:SPKI'
+    XMLDSIG                        = 'urn:oasis:names:tc:SAML:2.0:ac:classes:XMLDSig'
+    SMARTCARD                      = 'urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard'
+    SOFTWARE_PKI                   = 'urn:oasis:names:tc:SAML:2.0:ac:classes:SoftwarePKI'
+    TELEPHONY                      = 'urn:oasis:names:tc:SAML:2.0:ac:classes:Telephony'
+    NOMAD_TELEPHONY                = 'urn:oasis:names:tc:SAML:2.0:ac:classes:NomadTelephony'
+    PERSONAL_TELEPHONY             = 'urn:oasis:names:tc:SAML:2.0:ac:classes:PersonalTelephony'
+    AUTHENTICATED_TELEPHONY        = 'urn:oasis:names:tc:SAML:2.0:ac:classes:AuthenticatedTelephony'
+    SECURED_REMOTE_PASSWORD        = 'urn:oasis:names:tc:SAML:2.0:ac:classes:SecureRemotePassword'
+    TLS_CLIENT                     = 'urn:oasis:names:tc:SAML:2.0:ac:classes:TLSClient'
+    TIME_SYNC_TOKEN                = 'urn:oasis:names:tc:SAML:2.0:ac:classes:TimeSyncToken'
 
     ALL_CLASS_REFS     = [UNSPECIFIED,
                           PASSWORD_PROTECTED,
                           MOBILE_TWO_FACTOR_UNREGISTERED,
                           MOBILE_TWO_FACTOR_CONTRACT,
-                          MOBILE_SMARTCARD_PKI]
+                          MOBILE_SMARTCARD_PKI,
+                          INTERNET_PROTOCOL,
+                          INTERNET_PROTOCOL_PASSWORD,
+                          KERBEROS,
+                          MOBILE_ONE_FACTOR_UNREGISTERED,
+                          MOBILE_ONE_FACTOR_CONTRACT    ,
+                          PASSWORD,
+                          PREVIOUS_SESSION,
+                          X509,
+                          PGP,
+                          SPKI,
+                          XMLDSIG,
+                          SMARTCARD,
+                          SOFTWARE_PKI,
+                          TELEPHONY,
+                          NOMAD_TELEPHONY,
+                          PERSONAL_TELEPHONY,
+                          AUTHENTICATED_TELEPHONY,
+                          SECURED_REMOTE_PASSWORD,
+                          TLS_CLIENT,
+                          TIME_SYNC_TOKEN]
     ORDERED_CLASS_REFS = ALL_CLASS_REFS
   end
 
