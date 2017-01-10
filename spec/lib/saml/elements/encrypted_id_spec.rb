418c0a14ed2c1c2dfc62bccd4f864f92adb4abe1
Danny van der Heiden <dvanderheiden@digidentity.com>
2017-01-10 15:48:55 +0100

    Encrypt an EncryptedID for multiple key descriptors by adding an EncryptedKey for each key descriptor.

--- spec/lib/saml/elements/encrypted_id_spec.rb
+++ spec/lib/saml/elements/encrypted_id_spec.rb
@@ -52,41 +52,40 @@ describe Saml::Elements::EncryptedID do
   end
 
   describe '#encrypt' do
-    let(:name_id) { Saml::Elements::NameId.new(value: 'NAAM') }
-    let(:key_name) { '22cd8e9f32a7262d2f49f5ccc518ccfbf8441bb8' }
+    let(:name_id)           { Saml::Elements::NameId.new(value: 'NAAM') }
     let(:entity_descriptor) { Saml::Elements::EntityDescriptor.parse(File.read('spec/fixtures/metadata/service_provider.xml')) }
-    let(:key_descriptor) { entity_descriptor.sp_sso_descriptor.find_key_descriptor(key_name, 'encryption') }
-    let(:encrypted_id) { Saml::Elements::EncryptedID.new(name_id: name_id) }
+    let(:encrypted_id)      { Saml::Elements::EncryptedID.new(name_id: name_id) }
+    let(:key_descriptors)   { entity_descriptor.sp_sso_descriptor.find_key_descriptors_by_use('encryption') }
 
-    before { encrypted_id.encrypt(key_descriptor) }
+    context 'when a single key descriptor is given' do
+      before { encrypted_id.encrypt(key_descriptors.first) }
 
-    it 'builds #encrypted_data' do
-      expect(encrypted_id.encrypted_data).to be_a Xmlenc::Builder::EncryptedData
+      it 'encrypts the encrypted ID for the given key descriptor' do
+        aggregate_failures do
+          expect(encrypted_id.encrypted_data).to be_a Xmlenc::Builder::EncryptedData
+          expect(encrypted_id.encrypted_keys.count).to eq 1
+          expect(encrypted_id.encrypted_keys.first).to be_a Xmlenc::Builder::EncryptedKey
+          expect(encrypted_id.encrypted_keys.first.key_info.key_name).to eq '22cd8e9f32a7262d2f49f5ccc518ccfbf8441bb8'
+          expect(encrypted_id.name_id).to be_nil
+        end
+      end
     end
 
-    it 'builds #encrypted_keys' do
-      expect(encrypted_id.encrypted_keys).to be_a Array
-    end
+    context 'when multiple key descriptors are given' do
+      before { encrypted_id.encrypt(key_descriptors) }
 
-    it '#encrypted_keys are of type EncryptedKey' do
-      expect(encrypted_id.encrypted_keys.first).to be_a Xmlenc::Builder::EncryptedKey
-    end
-
-    it '#name_id is nil' do
-      expect(encrypted_id.name_id).to eq nil
+      it 'encrypts the encrypted ID for each given key descriptor' do
+        aggregate_failures do
+          expect(encrypted_id.encrypted_data).to be_a Xmlenc::Builder::EncryptedData
+          expect(encrypted_id.encrypted_keys.count).to eq 2
+          expect(encrypted_id.encrypted_keys.first).to be_a Xmlenc::Builder::EncryptedKey
+          expect(encrypted_id.encrypted_keys.first.key_info.key_name).to eq '22cd8e9f32a7262d2f49f5ccc518ccfbf8441bb8'
+          expect(encrypted_id.encrypted_keys.second).to be_a Xmlenc::Builder::EncryptedKey
+          expect(encrypted_id.encrypted_keys.second.key_info.key_name).to eq '82cd8e9f32a7262d2f49f5ccc518ccfbf8441bb8'
+          expect(encrypted_id.name_id).to be_nil
+        end
+      end
     end
   end
 
-  context 'when <xenc:EncryptedKey> is contained inside <xenc:EncryptedData>' do
-    let(:xml) { File.read File.join('spec', 'fixtures', 'encrypted_id_alternative.xml') }
-    subject   { described_class.parse(xml, single: true) }
-
-    it 'should parse the encrypted data' do
-      expect(subject.encrypted_data).to be_a Xmlenc::Builder::EncryptedData
-    end
-
-    it 'should not have any encrypted key' do
-      expect(subject.encrypted_keys).to be_empty
-    end
-  end
 end
