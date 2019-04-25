54f5862de2e53a3c5597570d0b2eda6fb38e922e
Danny van der Heiden <dannyvdheiden@gmail.com>
2019-04-25 11:46:13 +0200

    Let’s use the latest version of “factory_bot_rails” gem and fix the deprecation warnings regarding static attributes.
    “DEPRECATION WARNING: Static attributes will be removed in FactoryBot 5.0. Please use dynamic
    attributes instead by wrapping the attribute value in a block”

--- spec/factories/all.rb
+++ spec/factories/all.rb
@@ -52,39 +52,39 @@ end
 
 FactoryBot.define do
   factory :request_abstract_type_dummy, :class => RequestAbstractTypeDummy do
-    _id "_#{Time.now.to_i}"
-    version "2.0"
+    _id           { "_#{Time.now.to_i}" }
+    version       { "2.0" }
     issue_instant { Time.now }
-    issuer "http://sp.example.com"
-    destination "http://test.url/sso"
+    issuer        { "http://sp.example.com" }
+    destination   { "http://test.url/sso" }
   end
 
   factory :status_code, :class => Saml::Elements::StatusCode do
-    value Saml::TopLevelCodes::SUCCESS
+    value { Saml::TopLevelCodes::SUCCESS }
   end
 
   factory :status_detail, :class => Saml::Elements::StatusDetail do
-    status_value 'foo_status_value'
+    status_value { 'foo_status_value' }
   end
 
   factory :status, :class => Saml::Elements::Status do
-    status_code FactoryBot.build(:status_code)
-    status_detail FactoryBot.build(:status_detail)
+    status_code   { FactoryBot.build(:status_code) }
+    status_detail { FactoryBot.build(:status_detail) }
   end
 
   factory :status_response_type_dummy, :class => StatusResponseTypeDummy do
-    _id "_#{Time.now.to_i}"
-    version "2.0"
-    issue_instant { Time.now }
-    in_response_to "_#{Time.now.to_i}"
-    status FactoryBot.build(:status)
+    _id             { "_#{Time.now.to_i}" }
+    version         { "2.0" }
+    issue_instant   { Time.now }
+    in_response_to  { "_#{Time.now.to_i}" }
+    status          { FactoryBot.build(:status) }
   end
 
   factory :role_descriptor_type_dummy, :class => RoleDescriptorTypeDummy do
   end
 
   factory :sso_descriptor_type_dummy, :class => SsoDescriptorTypeDummy do
-    protocol_support_enumeration Saml::ComplexTypes::RoleDescriptorType::PROTOCOL_SUPPORT_ENUMERATION
+    protocol_support_enumeration { Saml::ComplexTypes::RoleDescriptorType::PROTOCOL_SUPPORT_ENUMERATION }
   end
 
   factory :attribute_type_dummy, :class => AttributeTypeDummy do
@@ -94,10 +94,10 @@ FactoryBot.define do
   end
 
   factory :assertion, :class => Saml::Assertion do
-    _id "_#{Time.now.to_i}"
-    version "2.0"
+    _id           { "_#{Time.now.to_i}" }
+    version       { "2.0" }
     issue_instant { Time.now }
-    issuer "valid_issuer"
+    issuer        { "valid_issuer" }
   end
 
   factory :encrypted_assertion, class: Saml::Elements::EncryptedAssertion do
@@ -137,7 +137,7 @@ FactoryBot.define do
 
   factory :authn_statement, :class => Saml::Elements::AuthnStatement do
     authn_instant { Time.now }
-    authn_context FactoryBot.build(:authn_context)
+    authn_context { FactoryBot.build(:authn_context) }
   end
 
   factory :requested_attribute, :class => Saml::Elements::RequestedAttribute do
@@ -156,7 +156,7 @@ FactoryBot.define do
   end
 
   factory :attribute_statement, :class => Saml::Elements::AttributeStatement do
-    attributes [ FactoryBot.build(:attribute) ]
+    attributes { [ FactoryBot.build(:attribute) ] }
   end
 
   factory :subject_locality, :class => Saml::Elements::SubjectLocality do
@@ -164,15 +164,15 @@ FactoryBot.define do
   end
 
   factory :requested_authn_context, :class => Saml::Elements::RequestedAuthnContext do
-    comparison 'minimum'
-    authn_context_class_ref Saml::ClassRefs::PASSWORD_PROTECTED
+    comparison              { 'minimum' }
+    authn_context_class_ref { Saml::ClassRefs::PASSWORD_PROTECTED }
   end
 
   factory :authn_request, :class => Saml::AuthnRequest, :parent => :request_abstract_type_dummy do
-    force_authn false
-    assertion_consumer_service_index 0
-    provider_name "provider name"
-    requested_authn_context FactoryBot.build(:requested_authn_context)
+    force_authn                       { false }
+    assertion_consumer_service_index  { 0 }
+    provider_name                     { "provider name" }
+    requested_authn_context           { FactoryBot.build(:requested_authn_context) }
   end
 
   factory :response, :class => Saml::Response, :parent => :status_response_type_dummy do
@@ -184,53 +184,53 @@ FactoryBot.define do
   end
 
   factory :artifact_resolve, :class => Saml::ArtifactResolve, :parent => :request_abstract_type_dummy do
-    artifact Saml::Artifact.new("AAQAAMh48/1oXIM+sDo7Dh2qMp1HM4IF5DaRNmDj6RdUmllwn9jJHyEgIi8=")
+    artifact { Saml::Artifact.new("AAQAAMh48/1oXIM+sDo7Dh2qMp1HM4IF5DaRNmDj6RdUmllwn9jJHyEgIi8=") }
   end
 
   factory :logout_request, :class => Saml::LogoutRequest, :parent => :request_abstract_type_dummy do
-    name_id "s00000000:12345678"
+    name_id { "s00000000:12345678" }
   end
 
   factory :logout_response, :class => Saml::LogoutResponse, :parent => :status_response_type_dummy do
   end
 
   factory :entity_descriptor, :class => Saml::Elements::EntityDescriptor do
-    entity_id "http://idp.example.com/metadata"
+    entity_id { "http://idp.example.com/metadata" }
   end
 
   factory :entities_descriptor, :class => Saml::Elements::EntitiesDescriptor do
-    entity_descriptors [FactoryBot.build(:entity_descriptor)]
-    entities_descriptors ["entities_descriptor"]
+    entity_descriptors   { [FactoryBot.build(:entity_descriptor)] }
+    entities_descriptors { ["entities_descriptor"] }
   end
 
   factory :key_descriptor, :class => Saml::Elements::KeyDescriptor do
-    certificate File.read("spec/fixtures/certificate.pem")
+    certificate { File.read("spec/fixtures/certificate.pem") }
   end
 
   factory :organization, :class => Saml::Elements::Organization do
   end
 
   factory :contact_person, :class => Saml::Elements::ContactPerson do
-    contact_type Saml::Elements::ContactPerson::ContactTypes::TECHNICAL
-    telephone_numbers ["0612345678"]
-    email_addresses ["technical@example.com"]
+    contact_type      { Saml::Elements::ContactPerson::ContactTypes::TECHNICAL }
+    telephone_numbers { ["0612345678"] }
+    email_addresses   { ["technical@example.com"] }
   end
 
   factory :publication_info, :class => Saml::Elements::PublicationInfo do
-    publisher "http://idp.example.com/metadata"
-    creation_instant { Time.now }
+    publisher         { "http://idp.example.com/metadata" }
+    creation_instant  { Time.now }
   end
 
   factory :subject_query_abstract_type_dummy, :class => SubjectQueryAbstractTypeDummy do
-    _id "_#{Time.now.to_i}"
-    version "2.0"
+    _id           { "_#{Time.now.to_i}" }
+    version       { "2.0" }
     issue_instant { Time.now }
-    subject FactoryBot.build(:subject)
+    subject       { FactoryBot.build(:subject) }
   end
 
   factory :attribute_query_type_dummy, :class => AttributeQueryTypeDummy do
-    _id "_#{Time.now.to_i}"
-    version "2.0"
+    _id           { "_#{Time.now.to_i}" }
+    version       { "2.0" }
     issue_instant { Time.now }
   end
 
@@ -239,11 +239,11 @@ FactoryBot.define do
   end
 
   factory :name_id, class: Saml::Elements::NameId do
-    value 'NameID'
+    value { 'NameID' }
   end
 
   factory :session_index, class: Saml::Elements::SessionIndex do
-    value 'SessionIndex'
+    value { 'SessionIndex' }
   end
 
   factory :scoping, :class => Saml::Elements::Scoping do
@@ -253,7 +253,7 @@ FactoryBot.define do
   end
 
   factory :idp_entry, :class => Saml::Elements::IdpEntry do
-    provider_id 'ProviderID'
+    provider_id { 'ProviderID' }
   end
 
 end
